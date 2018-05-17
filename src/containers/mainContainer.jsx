cdae1c4ff821b87e32961c8586fdcd098926bf7e
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-05-17 15:16:00 -0700

    Create private profile and public routes with React Router and Redirect

--- src/containers/mainContainer.jsx
+++ src/containers/mainContainer.jsx
@@ -6,30 +6,38 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../action/actions'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Redirect } from 'react-router-dom';
 
 const mapStateToProps = (store) => {
-
+  return {
+    userInfo: store.firstReducer.userInfo
+  }
 }
-
-const mapDispatchToProps = (dispatch) => {
-
-};
-
 
 class MainContainer extends Component {
 
   render() {
+    console.log(this.props.userInfo, '<----changing userInfo')
     return (
       <Router>
-      <div>
-      <h1 style={{textAlign: 'center'}}>PacMate</h1>
-    <Route path = '/suitcase/:location' render = {({match}) => <SuitcaseContainer location = {match.params.location} />} />
-        <Route exact={true} path = '/' component = {PublicContainer}/>
-      </div>
+        <div>
+          <h1 style={{ textAlign: 'center' }}>PacMate</h1>
+          <Route path='/suitcase/:location' render={({ match }) => <SuitcaseContainer location={match.params.location} />} />
+          <Route exact={true} path='/public' component={PublicContainer} />
+          <Route exact={true} path='/profile' component={ProfileContainer} />
+          <Route exact path="/" render={() => (
+            this.props.userInfo ? (
+              <Redirect to="/profile" />
+            ) : (
+              <PublicContainer />
+              )
+          )} />
+        </div>
       </Router>
     );
   }
 }
 
-export default connect(null, null)(MainContainer);
+export default connect(mapStateToProps, null)(MainContainer);
+
+
