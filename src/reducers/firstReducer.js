81f1c6f3dac479f7d921813bc112df76ce30bd45
Esther Kim <estherkim@MacBook-Air-2075.local>
2018-05-17 19:43:22 -0700

    demo

--- src/reducers/firstReducer.js
+++ src/reducers/firstReducer.js
@@ -5,9 +5,9 @@ const initialState = {
   {trip_id: 2, location: 'London', departure_date: 'Jun 15, 2018', return_date: 'Jun 25, 2018'},
   {trip_id: 3, location: 'Denver', departure_date: 'Jan 3, 2019', return_date: 'Jan 17, 2019'}],
   items: {
-  '1': ['contacts', 'phone', 'computer', 'dress', 'flats'],
-  '2': ['contacts', 'phone', 'computer', 'pants', 'jacket'],
-  '3': ['contacts', 'phone', 'computer', 'pants', 'jacket']}
+  '1': ['TO BRING:', 'contacts', 'glasses', 'phone', 'computer', 'black dress', 'flats', 'toothbrush', 'toothpaste', 'boots', 'fedora', '29 intimate wear', '29 socks', 'pyjamas', 'skincare', 'makeup'],
+  '2': ['TO BRING:', 'contacts', 'glasses', 'sunglasses', 'phone', 'computer', 'charger for both phone and computer', 'pants', 'jacket', 'toothbrush', 'toothpaste', 'sundress', 'skirts', 'distressed shorts', 'v-neck bodysuit', 'pyjamas', '29 intimate wear', 'skincare', 'makeup'],
+  '3': ['TO BRING:', 'contacts', 'glasses', 'sunglasses', 'phone', 'computer', 'charger for both phone and computer', 'pants', 'jacket', 'birkenstock sandals', 'flip-flops', 'bikini', 'dress', '29 intimate wear', 'pyjamas', 'skincare', 'makeup']}
 }
 
 //The action passed into our reducer comes from the dispatch.
